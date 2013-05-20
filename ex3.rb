a3b6f6f56e77f741cf02fc4a604da8c59b6f3ffb
Tom Leman <tomleman@gmail.com>
2013-05-20 11:52:16 +0100

    Ex3

---
+++ ex3.rb
@@ -0,0 +1,25 @@
+#Prints Text
+puts "I will now count my chickens:"
+#Prints hens and calculates 25 + (30/6)
+puts "Hens", 25 + 30 / 6
+#Prints Roosters and calculates
+puts "Roosters", 100 - 25 * 3 % 4
+#Prints Text
+puts "Now I will count the eggs:"
+#Calculates sum
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#Prints text
+puts "Is it true that 3 + 2 < 5 -7?"
+#Calculates sum
+puts 3 + 2 < 5 - 7
+#Prints text, then calculates sum
+puts "What is 3 + 2", 3 + 2
+puts "What is 5 - 7", 5 - 7
+#Prints
+puts "Oh, that's why it's false."
+#Prints
+puts "How about some more."
+#Prints text then calculates sums
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
