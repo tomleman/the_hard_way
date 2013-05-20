bd94f534b4a8b9feff9c98ebae465b43483044fa
Tom Leman <tomleman@gmail.com>
2013-05-20 15:17:56 +0100

    Ex13

---
+++ ex13.rb
@@ -0,0 +1,15 @@
+first, second, third = ARGV
+
+puts "Enter your first argument. "
+first=gets.chomp
+
+puts "Enter your second argument. "
+second=gets.chomp
+
+puts "Enter your third argument. "
+third=gets.chomp
+
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
