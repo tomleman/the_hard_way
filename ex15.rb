f7c9a250553cf68a844e5dfdbc9eb50cdc45f0b5
Tom Leman <tomleman@gmail.com>
2013-05-21 10:13:10 +0100

    Ex16

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,24 @@
+#Take the argument passed when the script is run
 filename = ARGV.first
 
+#Sets the symbol for prompts
 prompt = '> '
+#Runs file.open on the argument passed when the script was run
 txt = File.open(filename)
 
+#Outputs name of file in first argument
 puts "Here's your file: #{filename}"
-puts txt.read()
 
+#Takes the output of the txt function line 7 and runs .read on it
+puts txt.read
+
+#Prompts user to type the original filename again and prints prompt symbol
 puts "I'll also ask you to type it again:"
 print prompt
+#Takes user input of filename
 file_again = STDIN.gets.chomp()
-
+#Runs file.open on file for the second time
 txt_again = File.open(file_again)
 
-puts txt_again.read()
+#Takes the output from line 21 and displays it
+puts txt_again.read
